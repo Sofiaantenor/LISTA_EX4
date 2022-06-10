da05b8e43f6739fa603891b455a8b85c3b1501c0
Sofiaantenor <90793659+Sofiaantenor@users.noreply.github.com>
2022-06-10 15:39:04 -0300

    Add files via upload

---
+++ AMS_Ex4_22/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AMS_Ex4_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double grausCelsius = 32.5;
+            double fahrenheit = (9 * grausCelsius + 160) / 5;
+
+            Console.WriteLine("");
+            Console.WriteLine("    ***CONVERSÃO DE GRAUS CELSIUS PARA FAHRENHEIT***      ");
+
+            Console.WriteLine($"O valor equivalente a {grausCelsius} graus Celsius em Fahrenheit é {fahrenheit}");
+            Console.WriteLine("");
+        }
+    }
+}
